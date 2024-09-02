04c54eb0b70dd3e525f619b48071485feaf46873
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-02 15:11:18 +1000

    remove visual scrollbar

--- src/app/components/RestaurantsPanel.js
+++ src/app/components/RestaurantsPanel.js
@@ -3,10 +3,10 @@
 export default function RestaurantsPanel(){
 
     return (
-    <div className="w-full h-40 flex overflow-x-scroll space-x-4">
-        <div className="flex-none bg-slate-100 h-36 w-60 rounded-3xl"></div>
-        <div className="flex-none bg-slate-100 h-36 w-60 rounded-3xl"></div>
-        <div className="flex-none bg-slate-100 h-36 w-60 rounded-3xl"></div>
+    <div className="w-full h-40 flex overflow-x-scroll no-scrollbar">
+        <div className="flex-none bg-slate-100 h-36 w-60 rounded-3xl mx-2"></div>
+        <div className="flex-none bg-slate-100 h-36 w-60 rounded-3xl mx-2"></div>
+        <div className="flex-none bg-slate-100 h-36 w-60 rounded-3xl mx-2"></div>
     </div>
     );
 }
