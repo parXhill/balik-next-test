c5a9415666778a494744fc7743e3da0355f762b2
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-02 16:31:34 +1000

    Add keys to maps

--- src/app/restaurantCard/RestaurantCard.js
+++ src/app/restaurantCard/RestaurantCard.js
@@ -4,12 +4,12 @@ const RestaurantCard = ({restaurant}) => {
 
     function DealTag({tag}){
         return(
-            <p className="capitalize inline-block bg-slate-300 p-1 text-xs rounded-xl mr-2 px-2">{tag}</p>
+            <p key={tag} className="capitalize inline-block bg-slate-300 p-1 text-xs rounded-xl mr-2 px-2">{tag}</p>
         )
     }
 
     return (
-        <div className="relative flex-none flex flex-col bg-slate-100 h-48 w-64 rounded-3xl mx-2 overflow-y-scroll no-scrollbar">
+        <div key={restaurant.id} className="relative flex-none flex flex-col bg-slate-100 h-48 w-64 rounded-3xl mx-2 overflow-y-scroll no-scrollbar">
                 <img className="h-32 w-full object-cover"  src={restaurant.images} alt={restaurant.name}/>
                 <div className="absolute top-3 right-0 z-30 bg-blue-500 rounded-l-xl w-36 text-center text-xs text-white text-semibold py-1">{restaurant.deal[0].marker}     {restaurant.deal[0].shortDescription}!</div>
                 <div className="ml-5 grid grid-rows-2 grid-cols-[4fr_1fr]">
