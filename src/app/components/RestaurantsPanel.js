f6efb149a06808dbd98a8af06dff61a02a35e263
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-02 16:26:20 +1000

    Style RestaurantCards

--- src/app/components/RestaurantsPanel.js
+++ src/app/components/RestaurantsPanel.js
@@ -1,12 +1,19 @@
 'use client'
 
+import restaurantData from '../assets/ restaurant.json';
+import RestaurantCard from '../restaurantCard/RestaurantCard';
+
 export default function RestaurantsPanel(){
 
+    let restaurantArray = restaurantData.restaurants
+
+
+    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
     return (
-    <div className="w-full h-40 flex overflow-x-scroll no-scrollbar">
-        <div className="flex-none bg-slate-100 h-36 w-60 rounded-3xl mx-2"></div>
-        <div className="flex-none bg-slate-100 h-36 w-60 rounded-3xl mx-2"></div>
-        <div className="flex-none bg-slate-100 h-36 w-60 rounded-3xl mx-2"></div>
+    <div className="w-full h-48 flex overflow-x-scroll no-scrollbar mb-2">
+        {restaurantArray.map(restaurant => {return( <RestaurantCard restaurant={restaurant}/> )})}
+       
     </div>
     );
 }
--- src/app/restaurantCard/RestaurantCard.js
+++ src/app/restaurantCard/RestaurantCard.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
-const RestaurantCard = ({ restaurant, onClose }) => {
+const RestaurantCard = ({restaurant}) => {
+
+    function DealTag({tag}){
+        return(
+            <p className="capitalize inline-block bg-slate-300 p-1 text-xs rounded-xl mr-2 px-2">{tag}</p>
+        )
+    }
+
     return (
-        <div className="popup" onClick={onClose}>
-            <div className="popup-content" onClick={(e) => e.stopPropagation()}>
-                <button className="close-btn" onClick={onClose}>×</button>
-                <h2>{restaurant.name}</h2>
-                <img src={restaurant.images} alt={restaurant.name}/>
-                <p>{restaurant.description}</p>
-            </div>
+        <div className="relative flex-none flex flex-col bg-slate-100 h-48 w-64 rounded-3xl mx-2 overflow-y-scroll no-scrollbar">
+                <img className="h-32 w-full object-cover"  src={restaurant.images} alt={restaurant.name}/>
+                <div className="absolute top-3 right-0 z-30 bg-blue-500 rounded-l-xl w-36 text-center text-xs text-white text-semibold py-1">{restaurant.deal[0].marker}     {restaurant.deal[0].shortDescription}!</div>
+                <div className="ml-5 grid grid-rows-2 grid-cols-[4fr_1fr]">
+                    <div className="col-span-1 row-span-1 text-sm font-medium mt-1 text-nowrap">{restaurant.name}</div>
+                    <div className="col-span-1 row-span-1"></div>
+                    <div className="col-span-2 row-span-1">
+                        {restaurant.deal[0].tags.map(tag => {return(<DealTag tag={tag}/>)})}
+                    </div>
+                </div>
+                
         </div>
     );
 };
