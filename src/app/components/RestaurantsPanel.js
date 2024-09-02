579b2c45bc2a86e30841308f46a16dac219e3fab
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-02 16:31:01 +1000

    Add keys to mapped items

--- src/app/components/RestaurantsPanel.js
+++ src/app/components/RestaurantsPanel.js
@@ -8,8 +8,6 @@ export default function RestaurantsPanel(){
     let restaurantArray = restaurantData.restaurants
 
 
-    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
     return (
     <div className="w-full h-48 flex overflow-x-scroll no-scrollbar mb-2">
         {restaurantArray.map(restaurant => {return( <RestaurantCard restaurant={restaurant}/> )})}
