0c349f7d2c16d2821ea27b2aaa1a0a5b0df9516b
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-02 15:03:43 +1000

    Make restaurant panel scroll horizontally with overflow

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Map from "./components/Map.js";
 import NavigationBar from "./components/NavigationBar.js";
+import RestaurantsPanel from "./components/RestaurantsPanel.js";
 import Search from "./components/Search.js"
 
 export default function Home() {
@@ -8,9 +9,22 @@ export default function Home() {
     <>
       <div className="relative w-full h-screen">
         <Map className="absolute top-0 left-0 w-full h-full z-10" />
-        <Search/>     
+        <div className="absolute top-0 left-0 z-20 p-4">
+           <Search/> 
+        </div>   
       </div>
-         <NavigationBar/>
+
+      <div className="absolute bottom-16 left-0 z-20 w-full overflow-x-scroll">
+        <RestaurantsPanel/>
+      </div>
+      <div className="absolute bottom-0 left-0 z-20 w-full">
+        <NavigationBar/>
+      </div>
+      
+  
+
+
+        
    
     </>
   );
