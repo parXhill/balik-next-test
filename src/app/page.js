c437959603901231f466c6f812872b0749a1085c
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-02 14:06:21 +1000

    Move tailwind to components rather than in main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,17 @@
 import Image from "next/image";
 import Map from "./components/Map.js";
 import NavigationBar from "./components/NavigationBar.js";
+import Search from "./components/Search.js"
 
 export default function Home() {
   return (
     <>
       <div className="relative w-full h-screen">
         <Map className="absolute top-0 left-0 w-full h-full z-10" />
-        <div className="absolute top-0 left-0 z-20 p-4">
-          <p className="text-white bg-black bg-opacity-50 p-2 rounded">SearchBar</p>
-        </div>
+        <Search/>     
       </div>
-
-      <div className="absolute bottom-0 left-0 z-20 w-full">
          <NavigationBar/>
-      </div>
+   
     </>
   );
 }
