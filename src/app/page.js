1c921f5a39f00fa98f5babdf5f39b47aa78b4f67
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-02 13:32:22 +1000

    Make search bar visible over map

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,12 @@ import Map from "./components/Map.js";
 export default function Home() {
   return (
     <>
-  <div className="relative w-full h-screen">
-  <Map className="absolute top-0 left-0 w-full h-full z-10" />
-    <div className="relative z-20">
-      <p>SearchBar</p>
-    </div>
-  </div>
-  </>
+      <div className="relative w-full h-screen">
+        <Map className="absolute top-0 left-0 w-full h-full z-10" />
+        <div className="absolute top-0 left-0 z-20 p-4">
+          <p className="text-white bg-black bg-opacity-50 p-2 rounded">SearchBar</p>
+        </div>
+      </div>
+    </>
   );
 }
