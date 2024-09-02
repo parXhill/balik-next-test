bd2935eafdf489cd283bd3abad9582552a6d602b
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-02 16:49:11 +1000

    Added shadow to cards

--- src/app/restaurantCard/RestaurantCard.js
+++ src/app/restaurantCard/RestaurantCard.js
@@ -9,7 +9,7 @@ const RestaurantCard = ({restaurant}) => {
     }
 
     return (
-        <div className="relative flex-none flex flex-col bg-slate-100 h-48 w-64 rounded-3xl mx-2 overflow-y-scroll no-scrollbar">
+        <div className="relative flex-none flex flex-col bg-slate-100 h-48 w-64 rounded-3xl mx-2 overflow-y-scroll no-scrollbar shadow-xl">
                 <img className="h-32 w-full object-cover"  src={restaurant.images} alt={restaurant.name}/>
                 <div className="absolute top-3 right-0 z-30 bg-blue-500 rounded-l-xl w-36 text-center text-xs text-white text-semibold py-1">{restaurant.deal[0].marker}     {restaurant.deal[0].shortDescription}!</div>
                 <div className="ml-5 grid grid-rows-2 grid-cols-[4fr_1fr]">
