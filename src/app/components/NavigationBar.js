05dfd0e8a6ef180dcb096622760163049f8fbb89
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-09-06 10:16:12 +1000

    Remove home, account and ask buttons from navigation panel

--- src/app/components/NavigationBar.js
+++ src/app/components/NavigationBar.js
@@ -1,10 +1,7 @@
 'use client'
 
 import { PiBowlFoodBold } from "react-icons/pi";
-import { FiHome } from "react-icons/fi";
-import { CgProfile } from "react-icons/cg";
 import { GrTicket } from "react-icons/gr";
-import { HiOutlineChat } from "react-icons/hi";
 
 
 
@@ -22,21 +19,6 @@ export default function NavigationBar(){
             <div className="flex flex-col items-center justify-center">
                 <GrTicket  className="text-3xl font-extrabold"/>
                 <p className="text-xs">Deals</p>
-            </div>
-
-            <div className="flex flex-col items-center justify-center">
-                <FiHome className="text-3xl font-extrabold"/>
-                <p className="text-xs">Home</p>
-            </div>
-
-            <div className="flex flex-col items-center justify-center">
-                <CgProfile className="text-3xl font-extrabold"/>
-                <p className="text-xs">Account</p>
-            </div>
-
-            <div className="flex flex-col items-center justify-center">
-                <HiOutlineChat className="text-3xl font-extrabold"/>
-                <p className="text-xs">Ask</p>
             </div>
 
 
